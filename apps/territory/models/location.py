bf4777cab0481e20c2c25c118b6e470c8e1b39ef
PUYUP <hellopuyup@gmail.com>
2021-11-25 11:11:24 +0700

    fix territory location

--- apps/territory/models/location.py
+++ apps/territory/models/location.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Provinces(models.Model):
+class AbstractProvinces(models.Model):
     prov_id = models.IntegerField(primary_key=True, db_index=True)
     prov_name = models.CharField(max_length=255, db_index=True)
     locationid = models.IntegerField(db_index=True)
@@ -9,42 +9,46 @@ class Provinces(models.Model):
 
     class Meta:
         db_table = 'territory_provinces'
+        abstract = True
 
     def __str__(self) -> str:
         return self.prov_name
 
 
-class Cities(models.Model):
+class AbstractCities(models.Model):
     city_id = models.IntegerField(primary_key=True, db_index=True)
     city_name = models.CharField(max_length=255, db_index=True)
     prov_id = models.IntegerField(db_index=True)
 
     class Meta:
         db_table = 'territory_cities'
+        abstract = True
 
     def __str__(self) -> str:
         return self.city_name
 
 
-class Districts(models.Model):
+class AbstractDistricts(models.Model):
     dis_id = models.IntegerField(primary_key=True, db_index=True)
     dis_name = models.CharField(max_length=255, db_index=True)
     city_id = models.IntegerField(db_index=True)
 
     class Meta:
         db_table = 'territory_districts'
+        abstract = True
 
     def __str__(self) -> str:
         return self.dis_name
 
 
-class SubDistricts(models.Model):
+class AbstractSubDistricts(models.Model):
     subdis_id = models.IntegerField(primary_key=True, db_index=True)
     subdis_name = models.CharField(max_length=255, db_index=True)
     dis_id = models.IntegerField(db_index=True)
 
     class Meta:
         db_table = 'territory_subdistricts'
+        abstract = True
 
     def __str__(self) -> str:
         return self.subdis_name
--- apps/territory/models/models.py
+++ apps/territory/models/models.py
@@ -6,32 +6,32 @@ __all__ = list()
 
 
 if not is_model_registered('territory', 'Provinces'):
-    class Provinces(Provinces):
-        class Meta(Provinces.Meta):
+    class Provinces(AbstractProvinces):
+        class Meta(AbstractProvinces.Meta):
             pass
 
     __all__.append('Provinces')
 
 
 if not is_model_registered('territory', 'Cities'):
-    class Cities(Cities):
-        class Meta(Cities.Meta):
+    class Cities(AbstractCities):
+        class Meta(AbstractCities.Meta):
             pass
 
     __all__.append('Cities')
 
 
 if not is_model_registered('territory', 'Districts'):
-    class Districts(Districts):
-        class Meta(Districts.Meta):
+    class Districts(AbstractDistricts):
+        class Meta(AbstractDistricts.Meta):
             pass
 
     __all__.append('Districts')
 
 
 if not is_model_registered('territory', 'SubDistricts'):
-    class SubDistricts(SubDistricts):
-        class Meta(SubDistricts.Meta):
+    class SubDistricts(AbstractSubDistricts):
+        class Meta(AbstractSubDistricts.Meta):
             pass
 
     __all__.append('SubDistricts')
